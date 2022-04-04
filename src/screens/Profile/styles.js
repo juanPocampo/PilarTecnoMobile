1ed7fa0410aaea8114a9a22a9c642f312c0a36b5
orJuanPablo <juanp.ocampo.22@gmail.com>
2022-04-04 00:26:21 -0300

    Profile Styles

--- src/screens/Profile/styles.js
+++ src/screens/Profile/styles.js
@@ -6,29 +6,36 @@ const height = Dimensions.get("window").height;
 
 export const styles = StyleSheet.create({
   container: {
+    height,
+    width,
     backgroundColor: "black",
   },
   mainContent: {
-    width,
-    height: height - 55,
+    flex: 1,
     padding: 5,
     flexDirection: "row",
     alignItems: "center",
     justifyContent: "center",
   },
   cardContainer: {
-    height: "80%",
-    width: "100%",
+    flex: 0.25,
     borderRadius: 15,
     flexDirection: "column",
     borderWidth: 2,
   },
+  infoContainer: {
+    flex: 0.5,
+    flexDirection: "row",
+    justifyContent: "space-around",
+  },
   column: {
-    height: "100%",
-    width: "50%",
+    flex: 0.6,
   },
-  usrName: {
-    width: "50%",
+  bio: {
+    flex: 0.1,
   },
-  names: { width: "50%" },
+  button: {
+    borderRadius: 5,
+    backgroundColor: "red",
+  },
 });
